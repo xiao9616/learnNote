6c0d0d0f23b5dd4d9f90f60f4a1596cdf667464d
18710760109 <18710760109@163.com>
2020-04-11 17:25:55 +0800

    update

---
+++ project/QLearning/main.py
@@ -0,0 +1,58 @@
+# =============================================
+# -*- coding: utf-8 -*-           
+# @Time    : 2020/4/11 下午2:05    
+# @Author  : xiao9616           
+# @Email   : 749935253@qq.com   
+# @File    : main.py         
+# @Software: PyCharm
+# ============================================
+import numpy as np
+import random
+
+r = np.array([[-1, -1, -1, -1, 0, -1], [-1, -1, -1, 0, -1, 100], [-1, -1, -1, 0, -1, -1], [-1, 0, 0, -1, 0, -1],
+              [0, -1, -1, 0, -1, 100], [-1, 0, -1, -1, 0, 100]])
+
+q = np.zeros([6, 6], dtype=np.float32)
+
+gamma = 0.8
+
+step = 0
+while step < 1000:
+    state = random.randint(0, 4)
+    print(state)
+    if state != 5:
+        next_state_list = []
+        for i in range(6):
+            if r[state, i] != -1:
+                next_state_list.append(i)
+        next_state = next_state_list[random.randint(0, len(next_state_list) - 1)]
+        qval = r[state, next_state] + gamma * max(q[next_state])
+        q[state, next_state] = qval
+        print(q)
+    print("============================================")
+    step += 1
+
+# 验证
+
+for i in range(10):
+    print("============================================")
+    print("第{}次验证".format(i + 1))
+    state = random.randint(0, 5)
+    print('机器人处于{}'.format(state))
+    count = 0
+    while state != 5:
+        if count > 20:
+            print('fail')
+            break
+        # 选择最大的q_max
+        q_max = q[state].max()
+
+        q_max_action = []
+        for action in range(6):
+            if q[state, action] == q_max:
+                q_max_action.append(action)
+
+        next_state = q_max_action[random.randint(0, len(q_max_action) - 1)]
+        print("the robot goes to " + str(next_state) + '.')
+        state = next_state
+        count += 1
