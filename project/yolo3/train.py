9cefd4cde18ed432f6a99ecf604074f9845f817c
xiao9616 <18710760109@163.com>
2020-04-25 00:05:07 +0800

    update

---
+++ project/yolo3/train.py
@@ -0,0 +1,17 @@
+# =============================================
+# -*- coding: utf-8 -*-           
+# @Time    : 2020/4/24 10:20 下午    
+# @Author  : xiao9616           
+# @Email   : 749935253@qq.com   
+# @File    : train.py         
+# @Software: PyCharm
+# ============================================
+
+from keras.models import Model
+from keras.layers import Input
+from yolo3.yolo3_Model import yolo3Model
+
+model_input = Input(shape=(416, 416, 3))
+model_output = yolo3Model(model_input, 3, 5)
+model = Model(model_input, model_output)
+model.summary()
---
+++ project/yolo3/yolo3/yolo3_Model.py
@@ -0,0 +1,88 @@
+# =============================================
+# -*- coding: utf-8 -*-           
+# @Time    : 2020/4/24 10:25 下午    
+# @Author  : xiao9616           
+# @Email   : 749935253@qq.com   
+# @File    : yolo3_Model.py         
+# @Software: PyCharm
+# ============================================
+from keras.layers.normalization import BatchNormalization
+from keras.layers import Conv2D
+from keras.layers.advanced_activations import LeakyReLU
+from keras.regularizers import l2
+from keras.layers import ZeroPadding2D
+from keras.layers import Add
+from keras.layers import UpSampling2D
+from keras.layers import Concatenate
+def conv(x, *args, **kwargs):
+    new_kwargs = {'kernel_regularizer': l2(5e-4), 'use_bias': False}
+    new_kwargs['padding'] = 'valid' if kwargs.get('strides') == (2, 2) else 'same'
+    new_kwargs.update(kwargs)
+    x = Conv2D(*args, **new_kwargs)(x)
+    return x
+
+
+def CBL(x, *args, **kwargs):
+    x = conv(x, *args, **kwargs)
+    x = BatchNormalization()(x)
+    x = LeakyReLU(alpha=0.1)(x)
+    return x
+
+
+def PCBL(x, num_filters):
+    x = ZeroPadding2D(((1, 0), (1, 0)))(x)
+    x = CBL(x, num_filters, (3, 3), strides=(2, 2))
+    return x
+
+
+def CBLR(x, num_filters):
+    y = CBL(x, num_filters, (1, 1))
+    y = CBL(y, num_filters * 2, (3, 3))
+    x = Add()([x, y])
+    return x
+
+
+def CBL5(x, num_filters):
+    x = CBL(x, num_filters, (1, 1))
+    x = CBL(x, num_filters * 2, (3, 3))
+    x = CBL(x, num_filters, (1, 1))
+    x = CBL(x, num_filters * 2, (3, 3))
+    x = CBL(x, num_filters, (1, 1))
+    return x
+
+def CBLC(x, num_filters, out_filters):
+    x = CBL(x, num_filters * 2, (3, 3))
+    x = conv(x, out_filters, (1, 1))
+    return x
+
+def CBLU(x,num_filters):
+    x=CBL(x,num_filters,(1,1))
+    x=UpSampling2D(1)(x)
+    return x
+
+def yolo3Model(inputs, num_anchors, num_classes):
+    x = CBL(inputs, 32, (3, 3))
+    out = []
+    n = [1, 2, 8, 8, 4]
+    for i in range(5):
+        x = PCBL(x, 2 ** (6 + i))
+        for _ in range(n[i]):
+            x = CBLR(x, 2 ** (5 + i))
+            if i in [2, 3, 4]:
+                out.append(x)
+    x1 = CBL5(out[2], 512)
+    y1 = CBLC(x, 512, num_anchors * (num_classes + 5))
+
+    x=CBLU(x1,256)
+    x=Concatenate()([x,out[1]])
+
+    x2=CBL5(x,256)
+    y2=CBLC(x2,256,num_anchors * (num_classes + 5))
+
+    x = CBLU(x2, 128)
+    x = Concatenate()([x, out[0]])
+
+    x3=CBL5(x,128)
+    y3=CBLC(x3,128,num_anchors * (num_classes + 5))
+
+    return [y1,y2,y3]
